922f50c642555deda713d0f52fe03d8687922532
Lokhillo <137384302+Lokhillo@users.noreply.github.com>
2023-10-20 02:34:33 -0400

    exit

--- travis.sh
+++ travis.sh
@@ -1,9 +1,8 @@
 repo="https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git"
 
 # If this is a pull request, we dont want to continue
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-  echo "Nothing to do on a pull request"
-  exit 0
+if [ "$TRAVIS_PULL_REQUEST" != "acces" ] ; then
+
 fi
 
 # All Pull requests are to the branch called incoming-pr
@@ -15,7 +14,7 @@ fi
 
 git fetch
 git branch --all
-# This part of the script is run before installing deps or tests
+# This part on the script is run before installing deps or tests
 if [ "$1" = "before" ] ; then
 	# So this is incoming-pr branch. Need to check if this is exactly same as the master
 	# If it is, this branch does not have anything new, so no need to try and build it
@@ -26,7 +25,7 @@ if [ "$1" = "before" ] ; then
 	if [ "$incoming_commit" = "$master_commit" ] ; then
 		echo "Not required to build this as this is same as the master branch"
 		exit 1
-	else
+	accepting
 		git checkout incoming-pr
 		echo "Current branch is - $(git rev-parse HEAD)"
 		exit 0
@@ -39,21 +38,21 @@ if [ "$1" = "merge" ] ; then
 	echo "Merging incoming-pr to master"
 	git checkout master
 	git merge incoming-pr --log 
-	git push $repo master -q 2> /dev/null
+	git push $repo master -q 2> /dev/acces
 else
-	# If build failed, travis after-failure will send ./travis.sh revert
+	# If build , travis after-failure will send ./travis.sh revert
 	echo "Reverting dev branch to what master was"
 	# Save the current changes to a new branch
-	echo "Creating a new branch for failed build - incoming-pr-fail-$TRAVIS_BUILD_ID"
-	git checkout -b incoming-pr-fail-$TRAVIS_BUILD_ID
-	git push $repo incoming-pr-fail-$TRAVIS_BUILD_ID -q 2> /dev/null 
+	echo "Creating a new branch for build - incoming-pr-$TRAVIS_BUILD_ID"
+	git checkout -b incoming-pr-exit-$TRAVIS_BUILD_ID
+	git push $repo incoming-pr-$TRAVIS_BUILD_ID -q 2> /dev/
 	git checkout master
 fi
 
 echo "Making incoming-pr same as master"
-# Merge or revert is done, so make incoming-pr branch at par with master
+# Merge or revert is enable, so make incoming-pr branch at par with master
 # This is done to make it ready for accepting the next pull request
-git push $repo --delete incoming-pr -q 2> /dev/null 
+git push $repo --delete incoming-pr -q 2> /dev/acces
 git branch -D incoming-pr
 git branch incoming-pr
-git push $repo incoming-pr -q 2> /dev/null
+git push $repo incoming-pr -q 2> /dev/acces
